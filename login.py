2f6d8d3ac17538c26aca2435aa021b25e7f19556
bruno.silva <bruno.silva@alldax.com>
2022-05-13 16:27:45 -0300

    vuando

--- login.py
+++ login.py
@@ -1,10 +1,14 @@
 from PyQt5 import uic, QtWidgets
 import sqlite3
 from PyQt5.QtWidgets import QMessageBox
-from menu import Principal
+import time
+import getpass
+from planilhas.leitura import Ler_planilha
+from planilhas.escrevendo import Escrever
 
 class Login:
     def __init__(self):
+
         self.app = QtWidgets.QApplication([])
         self.login = uic.loadUi('QT/login_janela.ui')
         self.login.pushButton.clicked.connect(self.entrar)
@@ -15,9 +19,18 @@ class Login:
         self.cursor.execute('SELECT login, senha '
                             'FROM Logins')
         self.lista_logins = self.cursor.fetchall()
-        print(self.lista_logins)
+        # print(self.lista_logins)
 
+        #segubda janela
+        self.user = getpass.getuser()
+        self.app1 = QtWidgets.QApplication([])
         self.janela = uic.loadUi('QT/principal.ui')
+        self.janela.pushButton_3.clicked.connect(self.buscar)
+        self.janela.pushButton.clicked.connect(self.gerar)
+        self.janela.pushButton_2.clicked.connect(self.baixa)
+        self.arquivo = ''
+        self.text = ''
+
 
 
     def entrar(self):
@@ -26,24 +39,91 @@ class Login:
         conection = (f'{login}', f'{senha}')
         if login == '' or senha == '':
             if login == '':
-                QMessageBox.about(self.login, "ALERTA", "Digite o login por favor!!")
+                QMessageBox.about(self.login, "ALERTA", "Ei Colaborador, digite o usuario por favor!!")
 
             elif senha == '':
                 QMessageBox.about(self.login, "ALERTA", "Digite o senha por favor!!")
 
         else:
             if conection in self.lista_logins:
-                self.login.close()
-                # segunda janela
+                if self.login.radioButton.isChecked():
+                    print('fiscal')
+                if self.login.radioButton_2.isChecked():
+                    self.login.close()
+                    # segunda janela
+                    self.janela.show()
 
-                self.janela.show()
-
+                if self.login.radioButton_3.isChecked():
+                    print('Finaceiro')
+                if self.login.radioButton_4.isChecked():
+                    print('Dp')
             else:
-                QMessageBox.about(self.login, "ALERTA", "Login ou Senha INCORRETO")
+                LS_erro = QMessageBox()
+                LS_erro.setIcon(QMessageBox.Warning)
+                LS_erro.setText('Usuario ou Senha Incorreto')
+                LS_erro.setInformativeText('Por favor, insira um usuario e senha corretos')
+                LS_erro.setWindowTitle('Lofin Incorreto')
+                # LS_erro.setStandardButtons(QMessageBox.Ok)
+                LS_erro.setDefaultButton(QMessageBox.Ok)
+                LS_erro.exec_()
 
-class Janela2:
-    def __init__(self):
-        pass
+
+    def buscar(self):
+        self.arquivo = QtWidgets.QFileDialog.getOpenFileName()[0]
+        print(self.arquivo)
+        self.janela.lineEdit.setText(self.arquivo)
+        self.janela.label_2.setText('')
+
+    def gerar(self):
+        empresa = self.janela.comboBox.currentText()
+        print(empresa)
+        if empresa == "Tratamento Medware":
+            if self.arquivo == '':
+                QMessageBox.about(self.janela, "ALERTA",
+                                  "Nenhum arquivo selecionado, favor selecionar um arquivo  para gerar.")
+            else:
+                self.conciliar()
+        else:
+            QMessageBox.about(self.janela, "ALERTA",
+                              "Nenhuma modelo selecionada, favor selecionar um modelo  para gerar.")
+
+    def conciliar(self):
+
+        self.text, text_box = Ler_planilha(self.arquivo).ler()
+        # print(text)
+        time.sleep(2)
+        self.janela.label_2.setText('Gerando...')
+        i = len(self.text)
+        i = i / 100
+        i = str(i)
+        i = i.split('.')
+        i = int(i[0])
+        h = 100
+        print(i)
+        for linha in text_box:
+            # print(linha)
+            linha = str(linha)
+            self.janela.listWidget.addItem(f'{linha}')
+        for g in range(h):
+            time.sleep(0.01)
+            self.janela.progressBar.setValue(g + 1 + i)
+        time.sleep(2)
+        self.janela.label_2.setText('Arquivo Gerado')
+        time.sleep(4)
+        self.janela.progressBar.setValue(0)
+        self.janela.label_2.setText('Arquivo Gerado')
+
+    def baixa(self):
+        if self.text == '':
+            QMessageBox.about(self.janela, "ALERTA",
+                              "Nenhum arquivo foi gerado, favor gerar um arquivo para poder baixalo.")
+        else:
+            nome_arquivo = QtWidgets.QFileDialog.getSaveFileUrl()[0]
+            nome_arquivo = f'{nome_arquivo}'
+            nome_arquivo = nome_arquivo.replace('file:///', '')
+            print(nome_arquivo)
+            Escrever(nome_arquivo).escrevendo(self.text)
+
 
             
 
