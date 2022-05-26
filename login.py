f2b71c92e0acc4fbc873acb70ca532c1b29db517
bruno.silva <bruno.silva@alldax.com>
2022-05-26 14:28:26 -0300

    Codigo finalizado

--- login.py
+++ login.py
@@ -3,8 +3,6 @@ import sqlite3
 from PyQt5.QtWidgets import QMessageBox
 import time
 import getpass
-from planilhas.leitura import Ler_planilha
-from planilhas.escrevendo import Escrever
 
 class Login:
     def __init__(self):
@@ -13,11 +11,16 @@ class Login:
         self.login = uic.loadUi('QT/login_janela.ui')
         self.login.pushButton.clicked.connect(self.entrar)
 
+
+
+
+        self.user = getpass.getuser()
+
         #coneção ao banco para validação
-        db = sqlite3.connect('T:\DEPARTAMENTOS\AUTOMAÇÃO\DATABASES\jeremy\DB_jeremy.db')
+        db = sqlite3.connect(rf'C:\Users\{self.user}\Documents\RPA-tratar-planilhas\database\DBlogin.db')
         self.cursor = db.cursor()
         self.cursor.execute('SELECT login, senha '
-                            'FROM Logins')
+                            'FROM LOGINS')
         self.lista_logins = self.cursor.fetchall()
         # print(self.lista_logins)
 
@@ -25,11 +28,14 @@ class Login:
         self.user = getpass.getuser()
         self.app1 = QtWidgets.QApplication([])
         self.janela = uic.loadUi('QT/principal.ui')
+        lista_opcao1 = ['Escolha uma Opção', 'Opção1', 'Opção2']
+        self.janela.comboBox.addItems(lista_opcao1)
         self.janela.pushButton_3.clicked.connect(self.buscar)
         self.janela.pushButton.clicked.connect(self.gerar)
         self.janela.pushButton_2.clicked.connect(self.baixa)
+        self.janela.pushButton_4.clicked.connect(self.confirmar_empresa)
         self.arquivo = ''
-        self.text = ''
+        self.lista_depara = ''
 
 
 
@@ -39,7 +45,7 @@ class Login:
         conection = (f'{login}', f'{senha}')
         if login == '' or senha == '':
             if login == '':
-                QMessageBox.about(self.login, "ALERTA", "Ei Colaborador, digite o usuario por favor!!")
+                QMessageBox.about(self.login, "ALERTA", "Ei, digite o usuario por favor!!")
 
             elif senha == '':
                 QMessageBox.about(self.login, "ALERTA", "Digite o senha por favor!!")
@@ -47,26 +53,55 @@ class Login:
         else:
             if conection in self.lista_logins:
                 if self.login.radioButton.isChecked():
-                    print('fiscal')
+                    print('opção1')
                 if self.login.radioButton_2.isChecked():
                     self.login.close()
                     # segunda janela
                     self.janela.show()
 
                 if self.login.radioButton_3.isChecked():
-                    print('Finaceiro')
+                    print('opção2')
                 if self.login.radioButton_4.isChecked():
-                    print('Dp')
+                    print('opção3')
             else:
                 LS_erro = QMessageBox()
                 LS_erro.setIcon(QMessageBox.Warning)
                 LS_erro.setText('Usuario ou Senha Incorreto')
                 LS_erro.setInformativeText('Por favor, insira um usuario e senha corretos')
-                LS_erro.setWindowTitle('Lofin Incorreto')
+                LS_erro.setWindowTitle('Login Incorreto')
                 # LS_erro.setStandardButtons(QMessageBox.Ok)
                 LS_erro.setDefaultButton(QMessageBox.Ok)
                 LS_erro.exec_()
 
+    def confirmar_empresa(self):
+
+        self.empresa = self.janela.comboBox.currentText()
+        print(self.empresa)
+        print(self.janela.comboBox_2.currentText())
+
+        if self.empresa == 'Escolha uma Opção':
+            QMessageBox.about(self.janela, "ALERTA",
+                              "Voce não selecionou nenhuma empresa, favor selecionar uma empresa.")
+        elif self.empresa == 'Opção1':
+            if self.janela.comboBox_2.currentText() == '':
+                lista_acao = ["Escolha uma Opreação para Opção1", "Opção1.1", "Opção1.2"]
+                self.janela.comboBox_2.addItems(lista_acao)
+
+            elif self.janela.comboBox_2.currentText() == 'Escolha uma Opreação para Opção2' or "Opção2.1" or "Opção2.2":
+                self.janela.comboBox_2.clear()
+                lista_acao = ["Escolha uma Opreação para Opção1", "Opção1.1", "Opção1.2"]
+                self.janela.comboBox_2.addItems(lista_acao)
+                print(self.janela.comboBox_2.currentText())
+
+        elif self.empresa == "Opção2":
+            if self.janela.comboBox_2.currentText() == '':
+                lista_acao = ["Escolha uma Opreação para Opção2", "Opção2.1", "Opção2.2"]
+                self.janela.comboBox_2.addItems(lista_acao)
+
+            elif self.janela.comboBox_2.currentText() == 'Escolha uma Opreação para Opção1' or "Opção1.1" or "Opção1.2":
+                self.janela.comboBox_2.clear()
+                lista_acao = ["Escolha uma Opreação para Opção2", "Opção2.1", "Opção2.2"]
+                self.janela.comboBox_2.addItems(lista_acao)
 
     def buscar(self):
         self.arquivo = QtWidgets.QFileDialog.getOpenFileName()[0]
@@ -75,57 +110,37 @@ class Login:
         self.janela.label_2.setText('')
 
     def gerar(self):
-        empresa = self.janela.comboBox.currentText()
-        print(empresa)
-        if empresa == "Tratamento Medware":
-            if self.arquivo == '':
-                QMessageBox.about(self.janela, "ALERTA",
-                                  "Nenhum arquivo selecionado, favor selecionar um arquivo  para gerar.")
-            else:
-                self.conciliar()
-        else:
+        self.empresa = self.janela.comboBox.currentText()
+        print(self.empresa)
+        if self.arquivo == '':
             QMessageBox.about(self.janela, "ALERTA",
-                              "Nenhuma modelo selecionada, favor selecionar um modelo  para gerar.")
+                              "Nenhum arquivo selecionado, favor selecionar um arquivo  para continuar.")
 
-    def conciliar(self):
+        else:
+            print('Entrou na Opção')
+            if self.empresa == 'Opção1':
+                print('entrou na Opção1.2')
+                self.Opcao1()
 
-        self.text, text_box = Ler_planilha(self.arquivo).ler()
-        # print(text)
-        time.sleep(2)
-        self.janela.label_2.setText('Gerando...')
-        i = len(self.text)
-        i = i / 100
-        i = str(i)
-        i = i.split('.')
-        i = int(i[0])
-        h = 100
-        print(i)
-        for linha in text_box:
-            # print(linha)
-            linha = str(linha)
-            self.janela.listWidget.addItem(f'{linha}')
-        for g in range(h):
-            time.sleep(0.01)
-            self.janela.progressBar.setValue(g + 1 + i)
-        time.sleep(2)
-        self.janela.label_2.setText('Arquivo Gerado')
-        time.sleep(4)
-        self.janela.progressBar.setValue(0)
-        self.janela.label_2.setText('Arquivo Gerado')
+            elif self.empresa == 'Opção2':
+
+                self.Opcao2()
+
+            elif self.empresa == 'Escolha uma Opção':
+                QMessageBox.about(self.janela, "ALERTA",
+                                  "Nenhuma opção secundaria selecionada, favor selecionar alguma opção secundaria  para continuar.")
+
+    def Opcao1(self):
+
+        print('Opcao1')
+
+    def Opcao2(self):
+        print('Opcao2')
 
     def baixa(self):
-        if self.text == '':
-            QMessageBox.about(self.janela, "ALERTA",
-                              "Nenhum arquivo foi gerado, favor gerar um arquivo para poder baixalo.")
-        else:
-            nome_arquivo = QtWidgets.QFileDialog.getSaveFileUrl()[0]
-            nome_arquivo = f'{nome_arquivo}'
-            nome_arquivo = nome_arquivo.replace('file:///', '')
-            print(nome_arquivo)
-            Escrever(nome_arquivo).escrevendo(self.text)
 
-
-            
+        nome_arquivo = QtWidgets.QFileDialog.getSaveFileUrl()[0]
+        print('nome_arquivo')
 
 
 
--- menu.py
+++ menu.py
@@ -1,21 +1,56 @@
 from PyQt5 import uic, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 import getpass
-from planilhas.leitura import Ler_planilha
-from planilhas.escrevendo import Escrever
-import time
+
+
+
+
 
 class Principal:
     def __init__(self):
-
         self.user = getpass.getuser()
         self.app = QtWidgets.QApplication([])
         self.janela = uic.loadUi('QT/principal.ui')
+
+
+
         self.janela.pushButton_3.clicked.connect(self.buscar)
         self.janela.pushButton.clicked.connect(self.gerar)
         self.janela.pushButton_2.clicked.connect(self.baixa)
+        self.janela.pushButton_4.clicked.connect(self.confirmar_empresa)
         self.arquivo = ''
-        self.text = ''
+        self.lista_depara = ''
+
+    def confirmar_empresa(self):
+
+        self.empresa = self.janela.comboBox.currentText()
+        print(self.empresa)
+        print(self.janela.comboBox_2.currentText())
+
+        if self.empresa == 'Escolha uma Opção':
+            QMessageBox.about(self.janela, "ALERTA",
+                              "Voce não selecionou nenhuma empresa, favor selecionar uma empresa.")
+        elif self.empresa == 'Opção1':
+            if self.janela.comboBox_2.currentText() == '':
+                lista_acao = ["Escolha uma Opreação para Opção1",  "Opção1.1", "Opção1.2"]
+                self.janela.comboBox_2.addItems(lista_acao)
+
+            elif self.janela.comboBox_2.currentText() == 'Escolha uma Opreação para Opção2' or "Opção2.1" or "Opção2.2":
+                self.janela.comboBox_2.clear()
+                lista_acao = ["Escolha uma Opreação para Opção1",  "Opção1.1", "Opção1.2"]
+                self.janela.comboBox_2.addItems(lista_acao)
+                print(self.janela.comboBox_2.currentText())
+
+        elif self.empresa == "Opção2":
+            if self.janela.comboBox_2.currentText() == '':
+                lista_acao = ["Escolha uma Opreação para Opção2",  "Opção2.1", "Opção2.2"]
+                self.janela.comboBox_2.addItems(lista_acao)
+
+            elif self.janela.comboBox_2.currentText() == 'Escolha uma Opreação para Opção1' or "Opção1.1" or "Opção1.2":
+                self.janela.comboBox_2.clear()
+                lista_acao = ["Escolha uma Opreação para Opção2",  "Opção2.1", "Opção2.2"]
+                self.janela.comboBox_2.addItems(lista_acao)
+
 
     def buscar(self):
         self.arquivo = QtWidgets.QFileDialog.getOpenFileName()[0]
@@ -25,57 +60,43 @@ class Principal:
 
 
     def gerar(self):
-        empresa = self.janela.comboBox.currentText()
-        print(empresa)
-        if empresa == "MEDWARE":
-            if self.arquivo == '':
-                QMessageBox.about(self.janela, "ALERTA",
-                                  "Nenhum arquivo selecionado, favor selecionar um arquivo  para gerar.")
-            else:
-                self.conciliar()
-        else:
+        self.empresa = self.janela.comboBox.currentText()
+        print(self.empresa)
+        if self.arquivo == '':
             QMessageBox.about(self.janela, "ALERTA",
-                              "Nenhuma empresa selecionada, favor selecionar uma empresa  para gerar.")
+                              "Nenhum arquivo selecionado, favor selecionar um arquivo  para continuar.")
 
-    def conciliar(self):
+        else:
+            print('Entrou na Opção')
+            if self.empresa == 'Opção1':
+                print('entrou na Opção1.2')
+                self.Opcao1()
 
-        self.text, text_box = Ler_planilha(self.arquivo).ler()
-        # print(text)
-        time.sleep(2)
-        self.janela.label_2.setText('Gerando...')
-        i = len(self.text)
-        i = i / 100
-        i = str(i)
-        i = i.split('.')
-        i = int(i[0])
-        h = 100
-        print(i)
-        for linha in text_box:
-            # print(linha)
-            linha = str(linha)
-            self.janela.listWidget.addItem(f'{linha}')
-        for g in range(h):
-            time.sleep(0.01)
-            self.janela.progressBar.setValue(g + 1 + i)
-        time.sleep(2)
-        self.janela.label_2.setText('Arquivo Gerado')
-        time.sleep(4)
-        self.janela.progressBar.setValue(0)
-        self.janela.label_2.setText('Arquivo Gerado')
+            elif self.empresa == 'Opção2':
+
+                self.Opcao2()
+
+            elif self.empresa == 'Escolha uma Opção':
+                QMessageBox.about(self.janela, "ALERTA",
+                                  "Nenhuma opção secundaria selecionada, favor selecionar alguma opção secundaria  para continuar.")
+
+
+    def Opcao1(self):
+
+        print('Opcao1')
+
+    def Opcao2(self):
+        print('Opcao2')
 
     def baixa(self):
-        if self.text == '':
-            QMessageBox.about(self.janela, "ALERTA", "Nenhum arquivo foi gerado, favor gerar um arquivo para poder baixalo.")
-        else:
-            nome_arquivo = QtWidgets.QFileDialog.getSaveFileUrl()[0]
-            nome_arquivo = f'{nome_arquivo}'
-            print(nome_arquivo)
-            Escrever(nome_arquivo).escrevendo(self.text)
 
-
+        nome_arquivo = QtWidgets.QFileDialog.getSaveFileUrl()[0]
+        print('nome_arquivo')
 
 
 if __name__ == '__main__':
     p = Principal()
     p.janela.show()
     p.app.exec()
+
+
